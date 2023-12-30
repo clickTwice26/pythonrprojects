ce5f46b33a5c94fc921e34b4973d261b8fa0ea90
clickTwice26 <shagatoc@gmail.com>
2023-12-31 00:54:59 +0600

    project started

--- projectmanager.py
+++ projectmanager.py
@@ -1,11 +1,20 @@
 import os
 from json import loads, load, dumps, dump
 from datetime import datetime
+
+
+def currentime(wdm="both"):
+    now = datetime.now()
+    if wdm == "date":
+        return str(now).split(" ")[0]
+    if wdm == "time":
+        return str(now).split(" ")[1].split(".")[0]
+    elif wdm == "both":
+        return now
+
+
 currentWorkingDirectory = os.getcwd()
 project_configuration = load(open(f"{currentWorkingDirectory}/project_config.json", "r"))
 
-
 print(project_configuration)
-
-
-
+print(currentime("time"))
